ea7af09cae1b53a8f43fb39c5c5ff509cf741640
Aron Griffis <aron@scampersand.com>
2024-12-01 18:07:53 -0500

    tidy

--- src/day01.clj
+++ src/day01.clj
@@ -5,23 +5,17 @@
 
 (defn parse
   [input]
-  (->> (parse-longs input) (partition 2) (apply map vector)))
+  (->> (parse-longs input)
+       (partition 2)
+       (apply map vector)))
 
 (defn part-a
   [input]
   (let [[left right] (parse input)]
     (reduce + (map (comp abs -) (sort left) (sort right)))))
 
-(comment
-  (part-a (read-example "day01"))
-  (part-a (read-input "day01")))
-
 (defn part-b
   [input]
   (let [[left right] (parse input)
         mults (frequencies right)]
     (reduce + (map #(* % (get mults % 0)) left))))
-
-(comment
-  (part-b (read-example "day01"))
-  (part-b (read-input "day01")))
